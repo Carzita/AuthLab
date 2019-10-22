5f7d5ee6b344f70b83129c05152a8bb30d0c9943
Carzita <henrik.waersted@gmail.com>
2019-10-22 23:48:06 +0200

    lol

---
+++ src/main/java/authlab/Client.java
@@ -0,0 +1,82 @@
+package authlab;
+
+import java.net.MalformedURLException;
+import java.rmi.MarshalException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+public class Client
+{
+    public static void main( String[] args ) throws NotBoundException, MalformedURLException, RemoteException {
+        PrinterInterface service = (PrinterInterface) Naming.lookup("rmi://localhost:5099/printerTest");
+        Scanner myScanner = new Scanner(System.in);
+
+/*        while (true) {
+            System.out.println("Enter Username");
+            String username = myScanner.nextLine();
+            System.out.println("Enter password");
+            String password = myScanner.nextLine();
+            if(service.login(username, password)) {
+                break;
+            }
+        }*/
+
+        loop: while (true) {
+            System.out.println("Enter command, enter help for a list over commands");
+            String input = myScanner.nextLine();
+
+            switch (input.toLowerCase()) {
+                case "help":
+                    System.out.println(service.helpCommand());
+                    break;
+                case "print":
+                    System.out.println("Enter filename");
+                    String filename = myScanner.nextLine();
+                    System.out.println("Enter printer");
+                    String printer = myScanner.nextLine();
+                    System.out.println(service.print(filename, printer));
+                    break;
+                case "queue":
+                    System.out.println("<Job number> <File name>");
+                    service.queue().forEach(System.out::println);
+                    break;
+                case "topqueue":
+                    System.out.println("Enter job you want to be moved to top of the queue");
+                    if (myScanner.hasNextInt()) {
+                        int job = myScanner.nextInt();
+                        service.topQueue(job);
+                        break;
+                    } else {
+                        System.out.println("Only numbers accepted");
+                        myScanner.nextLine();
+                    }
+                    break;
+                case "start":
+                    System.out.println(service.start());
+                    break;
+                case "stop":
+                    System.out.println(service.stop());
+                    break;
+                case "restart":
+                    service.restart();
+                    break;
+                case "status":
+                    System.out.println(service.status());
+                    break;
+                case "readconfig":
+//                    service.readConfig();
+                    System.out.println("tba");
+                    break;
+                case "setconfig":
+//                    service.setConfig();
+                    System.out.println("tba2");
+                case "exit":
+                    break loop;
+                default:
+                    System.out.println("Unknown command, type 'help' for list over commands");
+            }
+        }
+    }
+}
---
+++ src/main/java/authlab/PrinterInterface.java
@@ -0,0 +1,21 @@
+package authlab;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface PrinterInterface extends Remote {
+
+    public String print (String filename, String printer) throws RemoteException;
+    public ArrayList<PrinterServant.queList> queue() throws RemoteException;
+    public void topQueue(int job) throws RemoteException;
+    public String start() throws RemoteException;
+    public String stop() throws RemoteException;
+    public void restart() throws RemoteException;
+    public String status() throws RemoteException;
+    public String readConfig(String parameter) throws RemoteException;
+    public void setConfig(String parameter, String value) throws RemoteException;
+    public String helpCommand() throws RemoteException;
+    public boolean login (String username, String password) throws  RemoteException;
+
+}
